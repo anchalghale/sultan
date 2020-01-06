bdb3fbfd4c61a8df7ad8707bdf2bceb80efd8bdd
Anchal Ghale <anchalghale@gmail.com>
2020-01-06 19:32:02 +0545

    Adds detection of gold

---
+++ lvision/gold.py
@@ -0,0 +1,13 @@
+# 629, 752, 7, 9
+import cv2
+from cutils import crop, imshow
+
+
+def get_gold(img, ocr):
+    img = crop(img, (629, 752, 35, 9))
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    gold = ''
+    for i in range(5):
+        cropped = crop(img, (i * 7, 0, 7, 9))
+        gold += f'{ocr.predict(cropped)}'
+    return f'Gold: {gold}'
