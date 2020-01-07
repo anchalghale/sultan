1a2039c9f090b313626c37752dc30d8915bfe283
Anchal Ghale <anchalghale@gmail.com>
2020-01-07 19:20:41 +0545

    Adds detection of summoner spell

---
+++ lvision/sums.py
@@ -0,0 +1,19 @@
+'''Module to detect summoner level'''
+import collections
+import cv2
+
+from cutils import crop
+
+Spells = collections.namedtuple('Spells', 'd f')
+
+
+def get_summoner_spells(image, ocr):
+    '''Finds summoner spell'''
+    img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    img = crop(img, (534, 707, 19, 16))
+    spell_d = ocr.predict(img)
+
+    img = crop(image, (558, 707, 19, 16))
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    spell_f = ocr.predict(img)
+    return Spells(spell_d, spell_f)
